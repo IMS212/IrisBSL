7b8bb562c242583a9070b90b8f601eaf55e1d4c7
IMS212 <ims@ims21.tech>
2021-11-27 20:00:30 -0800

    Initial refractor of blend mode storage

--- src/main/java/net/coderbot/iris/pipeline/DeferredWorldRenderingPipeline.java
+++ src/main/java/net/coderbot/iris/pipeline/DeferredWorldRenderingPipeline.java
@@ -13,6 +13,7 @@ import com.mojang.blaze3d.systems.RenderSystem;
 import net.coderbot.iris.Iris;
 import net.coderbot.iris.block_rendering.BlockRenderingSettings;
 import net.coderbot.iris.gl.blending.AlphaTestOverride;
+import net.coderbot.iris.gl.blending.BlendModeStorage;
 import net.coderbot.iris.gl.framebuffer.GlFramebuffer;
 import net.coderbot.iris.gl.program.Program;
 import net.coderbot.iris.gl.program.ProgramBuilder;
@@ -548,7 +549,7 @@ public class DeferredWorldRenderingPipeline implements WorldRenderingPipeline {
 			}
 
 			if (blendModeOverride != null) {
-				GlStateManager._blendFuncSeparate(blendModeOverride[0], blendModeOverride[1], blendModeOverride[2], blendModeOverride[3]);
+				BlendModeStorage.overrideBlend(blendModeOverride);
 			} else if (disableBlend) {
 				GlStateManager._disableBlend();
 			}
@@ -557,6 +558,9 @@ public class DeferredWorldRenderingPipeline implements WorldRenderingPipeline {
 		public void stopUsing() {
 			if (alphaTestOverride != null) {
 				AlphaTestOverride.teardown();
+			}
+			if (blendModeOverride != null) {
+				BlendModeStorage.restoreBlend();
 			}
 		}
 
