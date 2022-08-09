eb6bec7716136d4b734fd1e1630ccc1560f2cc37
IMS212 <ims@ims21.tech>
2022-08-08 22:13:48 -0700

    Merge Sodium 0.5 compat

--- src/sodiumCompatibility/java/net/coderbot/iris/compat/sodium/mixin/shadow_map/frustum/MixinAdvancedShadowCullingFrustum.java
+++ src/sodiumCompatibility/java/net/coderbot/iris/compat/sodium/mixin/shadow_map/frustum/MixinAdvancedShadowCullingFrustum.java
@@ -9,12 +9,20 @@ import org.spongepowered.asm.mixin.Shadow;
 @Mixin(AdvancedShadowCullingFrustum.class)
 public abstract class MixinAdvancedShadowCullingFrustum implements Frustum, FrustumAdapter {
 	@Shadow(remap = false)
-	public abstract boolean fastAabbTest(float minX, float minY, float minZ, float maxX, float maxY, float maxZ);
+	public abstract int fastAabbTest(float minX, float minY, float minZ, float maxX, float maxY, float maxZ);
 
 	@Override
 	public int testBox(float minX, float minY, float minZ, float maxX, float maxY, float maxZ) {
-		// TODO: Visibility.INSIDE (-2)
-		return fastAabbTest(minX, minY, minZ, maxX, maxY, maxZ) ? -1 : -3;
+		switch (fastAabbTest(minX, minY, minZ, maxX, maxY, maxZ)) {
+			case 0:
+				return -3;
+			case 1:
+				return -2;
+			case 2:
+				return -1;
+			default:
+				throw new IllegalStateException("Unknown visibility, this shouldn't be possible!");
+		}
 	}
 
 	@Override
