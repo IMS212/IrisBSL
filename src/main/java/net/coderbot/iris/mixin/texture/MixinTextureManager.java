678c69b00ecf2662eeccc4d521045643bd0e506b
IMS212 <ims@irisshaders.dev>
2023-10-24 09:50:03 -0700

    Fix many things

--- src/main/java/net/coderbot/iris/mixin/texture/MixinTextureManager.java
+++ src/main/java/net/coderbot/iris/mixin/texture/MixinTextureManager.java
@@ -13,7 +13,10 @@ import java.util.concurrent.Executor;
 
 @Mixin(TextureManager.class)
 public class MixinTextureManager {
-	@Inject(method = "m_118482_", at = @At("TAIL"))
+	@Inject(method = {
+		"m_118482_",
+		"method_18167"
+	}, at = @At("TAIL"), require = 1)
 	private void iris$onTailReloadLambda(ResourceManager resourceManager, Executor applyExecutor, Void void1, CallbackInfo ci) {
 		TextureFormatLoader.reload(resourceManager);
 		PBRTextureManager.INSTANCE.clear();
