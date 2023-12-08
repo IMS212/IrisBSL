cab8d4c5ab3245ef4e6ca7ecf5b7b88093a2df12
IMS212 <ims@irisshaders.dev>
2023-12-08 13:31:58 -0800

    Stub

---
+++ src/main/java/net/coderbot/iris/OculusStub.java
@@ -0,0 +1,10 @@
+package net.coderbot.iris;
+
+import net.minecraftforge.fml.ModLoadingContext;
+import net.minecraftforge.fml.common.Mod;
+
+@Mod("oculus")
+public class OculusStub {
+	public OculusStub() {
+	}
+}
